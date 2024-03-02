866fdf8d34e0b6f51509d5c951735bafefb1a6f1
Cristóbal Espinoza <caespinoza5@uc.cl>
2024-03-01 21:17:00 -0300

    testing fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -42,13 +42,13 @@ secret_name = f"<project>/{environment}"
 region_name = "us-east-1"
 
 # Create a Secrets Manager client
-session = boto3.session.Session()
-client = session.client(
-    service_name='secretsmanager',
-    region_name=region_name
-)
 
 try:
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
     get_secret_value_response = client.get_secret_value(
         SecretId=secret_name
     )
